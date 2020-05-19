c4bb97b1f97d5db5675a1dbafccf556f5bbbf833
Julius Passow <juliuspassow@web.de>
2020-05-19 16:40:24 +0200

    step by step

--- app/controllers/TasksController.rb
+++ app/controllers/TasksController.rb
@@ -1,24 +1,38 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @task.destroy
+    redirect_to task_path
   end
 end
